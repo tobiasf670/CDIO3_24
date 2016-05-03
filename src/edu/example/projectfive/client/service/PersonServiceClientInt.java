a3ea581da7b79fc4f411fe0aef143aad9ae72d77
Tobias Frantsen <tobias-frantsen@hotmail.com>
2016-05-03 10:59:37 +0200

    css

--- src/edu/example/projectfive/client/service/PersonServiceClientInt.java
+++
@@ -1,8 +0,0 @@
-/*
-package edu.example.projectfive.client.service;
-
-public interface PersonServiceClientInt {
-	void getPersonInfo();
-	void updatePersonInfo(String name, int age, String gender);
-}
-*/
